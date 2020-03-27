2f23b81b07bcd21668a298174144eaf28002e1f4
Bigaston <bigaston1@gmail.com>
2020-03-27 16:38:01 +0100

    Gestion des images

--- blogodo.js
+++ blogodo.js
@@ -20,7 +20,7 @@ app.get("/static/:file", (req, res) => {
 	if (fs.existsSync("./static/" + req.params.file)) {
 		res.sendFile(path.join(__dirname, "./static/" + req.params.file))
 	} else {
-		res.status(404).send(404)
+		res.status(404).send("Not Found")
 	}
 });
 
@@ -28,7 +28,17 @@ app.get("/p/:slug", (req, res) => {
 	if (fs.existsSync("./build/post/" + req.params.slug + ".html")) {
 		res.sendFile(path.join(__dirname, "./build/post/" + req.params.slug + ".html"))
 	} else {
-		res.status(404).send(404)
+		res.status(404).send("Not Found")
+	}
+})
+
+app.get("/img/*", (req, res) => {
+	img_url = req.path.replace("/img/", "")
+
+	if (fs.existsSync("./img/" + img_url)) {
+		res.sendFile(path.join(__dirname, "./img/" + img_url))
+	} else {
+		res.status(404).send("Not Found")
 	}
 })
 
@@ -36,7 +46,7 @@ app.get("/", (req, res) => {
 	if (fs.existsSync("./build/index.html")) {
 		res.sendFile(path.join(__dirname, "./build/index.html"))
 	} else {
-		res.status(404).send(404)
+		res.status(404).send("Not Found")
 	}
 })
 
