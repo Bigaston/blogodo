62b1d221b31472bb0cfb20d2c234ab42053ac35e
Bigaston <bigaston1@gmail.com>
2020-03-29 14:15:53 +0200

    SAuvegarde des informations dans render.js

--- modules/render.js
+++ modules/render.js
@@ -14,18 +14,39 @@ function plugin(opts){
 			banner: process.env.BANNER
 		}
 
+		var tag_obj = {}
+		var post_obj = {}
+
 		Object.keys(files).forEach(function(file){ 
 			files[file].contents = files[file].contents.toString()
 
-			tags = []
+			var tags = []
 			files[file].tags.replace(/ /g, "").split(",").forEach(t => {
 				tags.push({name: t})
 			})
 
-			files[file].slug = file.replace(".html", "")
+			var slug = file.replace(".html", "")
+			tags.forEach(t => {
+				if (tag_obj[t.name] == undefined) {
+					tag_obj[t.name] = [slug]
+				} else {
+					tag_obj[t.name].push(slug)
+				}
+			})
+
+			post_obj[slug] = {
+				title: files[file].title,
+				subtitle: files[file].subtitle,
+				tags: files[file].tags.replace(/ /g, "").split(","),
+				pubdate: files[file].pubdate
+			}
+
+			files[file].slug = slug
 			files[file].tags = tags
 			files[file].contents = Buffer.from(mustache.render(template, {site: site, article: files[file]}))
 		})
+
+		fs.writeFileSync("./build/db.json", JSON.stringify({tags: tag_obj, posts: post_obj}, null, 4))
 
 		done();
 	};
