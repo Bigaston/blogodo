6b1ad2354133ed27b118068ec841a46b643d6d56
Bigaston <bigaston1@gmail.com>
2020-06-26 16:05:08 +0200

    Fichier avec un . cachés

--- modules/file.js
+++ modules/file.js
@@ -3,6 +3,7 @@ const markdown = require('metalsmith-markdown');
 const render = require("./render")
 const render_index = require("./render_index")
 const rss = require("./rss")
+const clear_dot_file = require("./clear_dot_file")
 
 module.exports = {
 	render: () => {
@@ -11,6 +12,7 @@ module.exports = {
 		.source('../source')      
 		.destination('../build/post')
 		.clean(true)
+		.use(clear_dot_file())
 		.use(rss())                  
 		.use(markdown())
 		.use(render())
